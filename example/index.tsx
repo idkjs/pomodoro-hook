9cfa2fbde9d9f53840e2f367bc05fde736a98fb7
Alain Armand <aarmand.inbox@gmail.com>
2020-04-03 15:23:01 -0400

    deploy:demo

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,54 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ClockDemo } from './ClockDemo';
+import './App.css';
+import { usePomodoro } from '../dist';
+
+function ClockDemo({ initialTime }) {
+  const { startPomodoro, stopPomodoro, resetPomodoro, time } = usePomodoro(
+    initialTime,
+  );
+
+  return (
+    <main className="app manager">
+      <div className="timer-container">
+        <h1 id="time" className="countdown">
+          {time}
+        </h1>
+      </div>
+
+      <div>
+        <div className="options">
+          <div className="option">
+            <div className="button-groups">
+              <button
+                id="start-button"
+                className="button button--primary"
+                onClick={startPomodoro}
+              >
+                Start
+              </button>
+              <button
+                id="stop-button"
+                className="button button--danger"
+                onClick={stopPomodoro}
+              >
+                Stop
+              </button>
+              <button
+                id="reset-button"
+                className="button button--reset"
+                onClick={resetPomodoro}
+              >
+                Reset
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
 
 const App = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,2 @@
-import * as React from 'react';
-
-
 export { usePomodoro } from "./usePomodoro"
 
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
-export const Tick = () => {
-  return (
-    <div>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-};
